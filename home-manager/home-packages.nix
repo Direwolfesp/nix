f70dfce47e79872056a8af9a6f1beace1d35934d
Direwolfesp <alvarolg365@gmail.com>
2025-08-17 23:34:53 +0200

    added more home packages (dev tooling)

--- home-manager/home-packages.nix
+++ home-manager/home-packages.nix
@@ -4,8 +4,14 @@
   home.packages = with pkgs; [
     fastfetch
     fzf
+    jq
     fd
     ripgrep
+    nvtopPackages.amd
+    btop
+    tokei
+    tealdeer
+    eza
     yazi # TODO: remove from here
     nushell # TODO: remove from here
   ];
