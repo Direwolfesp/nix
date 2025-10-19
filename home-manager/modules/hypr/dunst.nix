67917123037240a70d97a373d88740dee6b35402
Direwolfesp <alvarolg365@gmail.com>
2025-10-19 15:45:12 +0200

    little dunst theme

--- home-manager/modules/hypr/dunst.nix
+++ home-manager/modules/hypr/dunst.nix
@@ -16,6 +16,40 @@ in
   config = lib.mkIf cfg.enable {
     services.dunst = {
       enable = true;
+      settings = {
+        global = {
+          font = "JetBrainsMono Nerd Font 10";
+          frame_color = "#82aaff";
+          separator_color = "frame";
+          padding = 16;
+          horizontal_padding = 16;
+          frame_width = 2;
+          corner_radius = 12;
+          background = "#1e1e2e";
+          foreground = "#cdd6f4";
+          transparency = 10;
+          icon_position = "left";
+          markup = "full";
+        };
+
+        urgency_low = {
+          background = "#1e1e2e";
+          foreground = "#a6adc8";
+          timeout = 5;
+        };
+
+        urgency_normal = {
+          background = "#1e1e2e";
+          foreground = "#cdd6f4";
+          timeout = 7;
+        };
+
+        urgency_critical = {
+          background = "#f38ba8";
+          foreground = "#1e1e2e";
+          timeout = 0;
+        };
+      };
     };
   };
 }
