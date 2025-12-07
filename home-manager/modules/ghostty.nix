17445f7a78c3a079925bcd4e679117b58d0fd40d
Direwolfesp <alvarolg365@gmail.com>
2025-12-07 15:05:32 +0100

    ghostty conf

--- home-manager/modules/ghostty.nix
+++ home-manager/modules/ghostty.nix
@@ -25,6 +25,7 @@ in
         background-opacity = 0.8;
         font-size = 15;
         gtk-tabs-location = "hidden";
+        gtk-single-instance = "true";
         confirm-close-surface = "false";
         app-notifications = "no-clipboard-copy";
 
