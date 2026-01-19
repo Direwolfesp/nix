fd6619067ef974465dc65d09147d9b021cfeb800
Direwolfesp <alvarolg365@gmail.com>
2026-01-19 10:07:25 +0100

    flake update

--- home-manager/modules/fastfetch_images.nix
+++ home-manager/modules/fastfetch_images.nix
@@ -3,25 +3,26 @@ let
   logoDir = "Pictures/fastfetch_logos";
 
   # Example image
-  fubuki = pkgs.fetchurl {
-    url = "https://pbs.twimg.com/media/GJDueQOXsAA7DjN?format=jpg&name=medium";
-    hash = "sha256-m7175RBqjgsDb+yNyRkR0AnVp9rvG1nrX7HKpqSJLSU=";
-  };
-
-  # Add more here...
   geass = pkgs.fetchurl {
     url = "https://i.postimg.cc/FHrV4Y0d/geass.png";
     hash = "sha256-ijbt0aP+qj6uVbzl67uowzl85q0Wxg10sEKLaQym274=";
   };
+
+  chariot = pkgs.fetchurl {
+    url = "https://w7.pngwing.com/pngs/279/963/png-transparent-jojo-s-bizarre-adventure-eyes-of-heaven-jojo-s-bizarre-adventure-all-star-battle-josuke-higashikata-silver-chariot-others.png";
+    hash = "sha256-7rfbCFhM+yU7dfLcponJsFQarK/xJge7FcpoNOVWPgQ=";
+  };
+
+  # Add more here...
 in
 {
   home.file = {
-    "${logoDir}/Fubuki.jpg" = {
-      source = fubuki;
-    };
-
     "${logoDir}/geass.png" = {
       source = geass;
+    };
+
+    "${logoDir}/chariot.png" = {
+      source = chariot;
     };
   };
 }
