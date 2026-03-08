f63dd1181dff9ab877ff28fd1b8446ceea5161f8
Direwolfesp <alvarolg365@gmail.com>
2026-03-08 11:36:05 +0100

    update flake and remove cava

--- nixos/dms-shell.nix
+++ nixos/dms-shell.nix
@@ -11,7 +11,7 @@
     enableSystemMonitoring = true; # System monitoring widgets (dgop)
     enableVPN = true; # VPN management widget
     enableDynamicTheming = true; # Wallpaper-based theming (matugen)
-    enableAudioWavelength = true; # Audio visualizer (cava)
+    enableAudioWavelength = false; # Audio visualizer (cava)
     enableCalendarEvents = true; # Calendar integration (khal)
   };
 }
