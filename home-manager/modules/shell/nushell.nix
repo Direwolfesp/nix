dc1b0330da30af5ea670867874cc728beef019b3
Direwolfesp <alvarolg365@gmail.com>
2026-04-10 00:07:42 +0200

    nushell manpage alias

--- home-manager/modules/shell/nushell.nix
+++ home-manager/modules/shell/nushell.nix
@@ -46,6 +46,20 @@ in
           do { |url|
             nix store prefetch-file --hash-type sha256 --json $url | from json | get hash
           }'';
+
+        # Man page fuzzy picker
+        m = ''
+         do {
+          let input = man -k . | lines | input list --fuzzy
+          if ($input | is-empty) {
+            return
+          }
+          let out = $input | split row ' '
+          let name = $out.0
+          let num = $out.1 | str substring 1..(-2)
+          man $num $name | bat -l man -p --theme gruvbox-dark
+        }
+        '';
       };
     };
 
